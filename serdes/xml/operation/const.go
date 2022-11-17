6c1b0b5d944ee88186a62b8a261ebb3e8d1c6715
kanade0404 <melty0404@gmail.com>
2022-11-17 21:06:16 +0900

    :sparkles: 打牌情報をparse

---
+++ serdes/xml/operation/const.go
@@ -0,0 +1,8 @@
+package operation
+
+const (
+	FirstUserIndex  = iota // 自身
+	SecondUserIndex        // 下家
+	ThirdUserIndex         // 対面
+	FourthUserIndex        // 上家
+)
---
+++ serdes/xml/operation/discard_test.go
@@ -0,0 +1,76 @@
+package operation
+
+import (
+	"github.com/google/go-cmp/cmp"
+	"github.com/kanade0404/tenhou-log/serdes/xml"
+	"github.com/kanade0404/tenhou-log/serdes/xml/hai"
+	"testing"
+)
+
+func TestDiscard(t *testing.T) {
+	west, err := hai.NewHai(119, true)
+	if err != nil {
+		panic(err)
+	}
+	oneBamboo, err := hai.NewHai(75, true)
+	if err != nil {
+		panic(err)
+	}
+	type args struct {
+		discardType string
+		haiID       uint
+		gameInfo    *xml.GameInfo
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *Discard
+		wantErr bool
+	}{
+		{
+			name: "親が西を捨てる",
+			args: args{
+				discardType: "D",
+				haiID:       119,
+				gameInfo: &xml.GameInfo{
+					Red: true,
+				},
+			},
+			want: &Discard{
+				discardUserIndex: 0,
+				hai:              west,
+				isRedRule:        true,
+			},
+		},
+		{
+			name: "南家が1索を捨てる",
+			args: args{
+				discardType: "U",
+				haiID:       75,
+				gameInfo: &xml.GameInfo{
+					Red: true,
+				},
+			},
+			want: &Discard{
+				discardUserIndex: 0,
+				hai:              oneBamboo,
+				isRedRule:        true,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := NewDiscard(tt.args.discardType, tt.args.haiID, tt.args.gameInfo)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("NewDiscard() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if diff := cmp.Diff(got.DiscardUserIndex(), tt.want.DiscardUserIndex()); diff != "" {
+				t.Errorf("(-got+want)\n%v", diff)
+			}
+			if diff := cmp.Diff(got.Hai().String(), tt.want.Hai().String()); diff != "" {
+				t.Errorf("(-got+want)\n%v", diff)
+			}
+		})
+	}
+}
